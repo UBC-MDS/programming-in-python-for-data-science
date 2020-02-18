580b29b2083d8c1121d24cf6923bb2e78f8de0f8
hayley <hayleyfboyce@hotmail.com>
2020-02-17 20:41:22 -0800

    add change to text

--- exercises/test_01_03a.py
+++ exercises/test_01_03a.py
@@ -8,7 +8,6 @@ def test():
 
     assert "import pandas" in __solution__ , "Are you using the right commands to import pandas?"
     assert "read.csv" in __solution__ , "Are you reading in the data properly?"
-    assert "canucks.csv" in __solution__ , "Are you reading in the correct data?"
     assert hockey_players.shape == (22, 10), "You may not have the correct dataset"
     assert list(hockey_players.columns) ==  ['Player', 'No.', 'Age', 'Height', 'Weight', 'Country ', 'Position', 'Experience', 'Birth Date', 'Salary'], "Your column names does not seem correct"
     __msg__.good("Nice work, well done!")
