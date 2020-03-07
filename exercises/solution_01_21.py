2ddb264975d2791af92a3458731c3fdabb4bd7ba
hayley <hayleyfboyce@hotmail.com>
2020-03-06 16:55:07 -0800

    still a lot to do but saving now

--- exercises/solution_01_21.py
+++ exercises/solution_01_21.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
+#ignore these imports for now
+
+from matplotlib import style
+style.use('seaborn-white')
+
 # Bring in previous frequency table we had saved as position_freq_df ignore this for now
 
 position_freq_df = pd.read_csv('data/position_freq_df.csv', index_col=0)
@@ -7,5 +12,8 @@ position_freq_df
 
 # Use `plot.bar()` with `position_freq`
 # Assign a `color` as `Teal`, set opacity to 0.5 and don't forget to add a title as "Canuck Player Positions"  
+position_bar = position_freq_df.plot.bar(color = "Teal", 
+                                         alpha = .5, 
+                                         title = "Canuck player positions")
 
-position_bar = position_freq_df.plot.bar(color = "Teal", alpha = .5, title = "Canuck player positions")
+position_bar
