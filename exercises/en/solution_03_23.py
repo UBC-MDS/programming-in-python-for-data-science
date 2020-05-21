ea4e9c7c410c841434f438099e75e4e8bdb224da
hayley <hayleyfboyce@hotmail.com>
2020-05-21 15:40:28 -0700

    more data

--- exercises/en/solution_03_23.py
+++ exercises/en/solution_03_23.py
@@ -3,7 +3,7 @@ import pandas as pd
 # We are going to import the dataframes lego_base and lego_opacity.
 
 lego_base = pd.read_csv('data/lego_theme_minimal.csv', index_col=0)
-lego_opacity = pd.read_csv('data/lego_opacity.csv',  index_col=0)
+lego_piece_type = pd.read_csv('data/lego_piece_type.csv',  index_col=0)
 
 # Combine the two dataframes horizontally to make 1 large complete dataframe using an inner join
 # Name the new dataframe lego_sets
@@ -21,5 +21,9 @@ lego_details = (lego_full.assign(total_pieces = lego_full['matte'] + lego_full['
 # Display the dataframe
 
 lego_details
+
+lego_piece_type
+
+lego_base
 
 
