71df0ba85b419cbd607949a11ef8b61ab28800cf
Tomas Beuzen <tomas.beuzen@gmail.com>
2020-09-16 00:09:48 -0700

    checking that netlify preview is stale

--- src/components/juniper.js
+++ src/components/juniper.js
@@ -16,7 +16,7 @@ class Juniper extends React.Component {
     static defaultProps = {
         children: '',
         branch: 'master',
-        url: 'https://binder.mds.rudaux.com',
+        url: 'https://mybinder.org',
         serverSettings: {},
         kernelType: 'python3',
         lang: 'python',
