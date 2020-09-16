82361f36faca36705af6f25eada979f05dd20826
Tomas Beuzen <40379073+TomasBeuzen@users.noreply.github.com>
2020-09-15 21:19:29 -0700

    secure binder link

--- src/components/juniper.js
+++ src/components/juniper.js
@@ -16,7 +16,7 @@ class Juniper extends React.Component {
     static defaultProps = {
         children: '',
         branch: 'master',
-        url: 'https://binder.rudaux.com/',
+        url: 'https://binder.rudaux.com',
         serverSettings: {},
         kernelType: 'python3',
         lang: 'python',
