e1dccf70316dce5061819d7c1bc12a04a2f641c2
ecool50 <ewillie@sfu.ca>
2020-09-21 13:46:19 -0700

    Update test_07_11.py

--- exercises/en/test_07_11.py
+++ exercises/en/test_07_11.py
@@ -6,7 +6,7 @@ def test():
     # in the meta.json for details.
 
     # If an assertion fails, the message will be displayed
-    assert 'test_find_force' in __solution__, 'Your function does not exist. Please make sure you are naming your function test_find_force.'
+    assert 'test_find_force()' in __solution__, 'Your function does not exist. Please make sure you are naming your function test_find_force().'
     str_fun = inspect.getsource(test_find_force)
     assert str_fun.count('assert') == 3, 'Make sure you are including all the assert statements.'
     assert str_fun.count('find_force(50, 3)') == 1, 'Make sure you are including all the assert statements.'
