c978a35d033b58ada520cce9abc2c5c6cd3abc54
hayley <hayleyfboyce@hotmail.com>
2020-09-27 14:34:57 -0700

    ficed 2.21

--- exercises/en/solution_02_21b.py
+++ exercises/en/solution_02_21b.py
@@ -5,7 +5,7 @@ pokemon = pd.read_csv('data/pokemon_sw.csv')
 
 # Create an object using single brackets to obtain the column base_score and name it bs_column
 
-bs_column = score_plot = pd.DataFrame(pokemon['base_score'])
+bs_column = pd.DataFrame(pokemon['base_score'])
 
 # Plot the object bs_column using .mark_bar() and save this graph as score_plot
 
