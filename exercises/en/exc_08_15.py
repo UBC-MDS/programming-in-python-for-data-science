76fd9b71215417d9c6919eb06378e78c8732c59d
Elijah Willie <ewillie@sfu.ca>
2020-09-28 12:35:38 -0700

    Made some minor changes

--- exercises/en/exc_08_15.py
+++ exercises/en/exc_08_15.py
@@ -5,18 +5,18 @@ canucks = pd.read_csv('data/canucks.csv')
 # Identify any columns with null values with .info()
 # Save this dataframe as canucks_info
 
-# ____ = ____
-# ____
+____ = ____
+____
 
 # Create a new column in the dataframe named Wealth
 # where all the values equal "comfortable"
 # Name the new dataframe canucks_comf
 
-# ____ = ____
-# ____
+____ = ____
+____
 
 # Do conditional replacement, where if the value in the salary column is null,
 # we replace "comfortable" with "unknown" 
 
-# ____ = ____
-# ____
+____ = ____
+____
